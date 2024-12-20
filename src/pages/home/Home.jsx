342403fc73853ced3555c3ef3b5be8049d0ad3cf
Muhammad Bin Sikandar <m.bin.sikandar@gmail.com>
2024-12-20 15:36:46 +0500

    TrustedBy Section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import "./Home.scss";
 import Featured from '../../components/featured/Featured';
+import TrustedBy from '../../components/trsutedBy/TrustedBy';
 
 const Home = () => {
     return (
         <div className = 'Home'>
             <Featured />
-            test
+            <TrustedBy />
         </div>
     );
 };
