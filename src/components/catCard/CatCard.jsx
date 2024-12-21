ca2b91a9b8e72274ad9c7c6b0a8c112bbd662dcb
Muhammad Bin Sikandar <m.bin.sikandar@gmail.com>
2024-12-21 11:14:03 +0500

    Slider Component and Hero Section added

---
+++ src/components/catCard/CatCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './CatCard.scss'
+import { Link } from 'react-router-dom'
+// import Slider from 'infinite-react-carousel'
+
+const CatCard = ({ item }) => {
+  return (
+    <Link to='/gigs?cat=design'>
+      <div className="catCard">
+        <img src={item.img} alt={item.title} />
+        <span className='desc'>{item.desc}</span>
+        <span className='title'>{item.title}</span>
+      </div>
+    </Link>
+  );
+};
+
+export default CatCard;
+
