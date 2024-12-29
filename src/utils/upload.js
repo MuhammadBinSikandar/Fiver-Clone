3541d3a4eb8cf22d85afff50f1b83f65bdae9094
Muhammad Bin Sikandar <m.bin.sikandar@gmail.com>
2024-12-29 11:51:15 +0500

    Query handling

--- src/utils/upload.js
+++ src/utils/upload.js
@@ -3,10 +3,11 @@ import axios from "axios";
 const upload = async (file) => {
     const data = new FormData();
     data.append("file", file);
-    data.append("upload_preset", "fiverr");
+    data.append("upload_preset", "Flexihub");
 
     try {
-        const res = await axios.post(import.meta.env.VITE_UPLOAD_LINK, data);
+        const res = await axios.post(
+            "https://api.cloudinary.com/v1_1/ddudykruo/image/upload", data);
 
         const { url } = res.data;
         return url;
